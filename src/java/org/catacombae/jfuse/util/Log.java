7a42fb256dc957de6303926fcd04f7dfec18c41a
Erik Larsson <catacombae@gmail.com>
2009-06-14 12:51:47 +0200

    Logging class added.

---
+++ src/java/org/catacombae/jfuse/util/Log.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.catacombae.jfuse.util;
+
+/**
+ *
+ * @author erik
+ */
+public class Log {
+    public static void traceEnter(String methodName, Object... args) {
+        StringBuilder sb = new StringBuilder("ENTER: ");
+        sb.append(methodName).append("(");
+        for(int i = 0; i < args.length; ++i) {
+            if(i != 0)
+                sb.append(", ");
+            sb.append(args[i].toString());
+        }
+        sb.append(")");
+        
+        System.err.println(sb.toString());
+    }
+    public static void traceExit(String methodName, Object retval, Object... args) {
+        StringBuilder sb = new StringBuilder("EXIT: ");
+        sb.append(methodName).append("(");
+        for(int i = 0; i < args.length; ++i) {
+            if(i != 0)
+                sb.append(", ");
+            sb.append(args[i].toString());
+        }
+        sb.append("): ").append(retval.toString());
+
+        System.err.println(sb.toString());
+    }
+
+    /**
+     * Free form TRACE level log message.
+     * @param msg
+     */
+    public static void trace(String msg) {
+        System.err.println("TRACE: " + msg);
+    }
+}
