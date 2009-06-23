4e524fdd8cbaff6a6fa99031f8fdb0000011573f
Erik Larsson <catacombae@gmail.com>
2009-06-23 20:56:27 +0200

    JNILoader now loads all dylibs with architecture/OS suffix present.

--- src/java/org/catacombae/jfuse/JNILoader.java
+++ src/java/org/catacombae/jfuse/JNILoader.java
@@ -10,6 +10,151 @@ package org.catacombae.jfuse;
  * @author erik
  */
 class JNILoader {
+    private static final String LIBRARY_BASE_NAME = "jfuse";
+
+    /**
+     * Set this variable to true if you want some messages printed to stderr when the library is
+     * loaded.
+     */
+    public static boolean verboseLoadLibrary = true;
+
+    private enum CPUArchitecture {
+
+        I386("i386"), AMD64("amd64"), IA64("ia64"),
+        POWERPC("ppc32"), POWERPC64("ppc64"),
+        SPARC("sparc32"), SPARC64("sparc64"),
+        MIPS("mips32"), MIPS64("mips64"), ALPHA("alpha"),
+        UNKNOWN;
+
+        private final String idString;
+
+        private CPUArchitecture() {
+            this.idString = null;
+        }
+
+        private CPUArchitecture(String idString) {
+            this.idString = idString;
+        }
+
+        public String getArchitectureString() {
+            return idString;
+        }
+    };
+
+    private enum ExecutionEnvironment {
+
+        WINDOWS("windows"), DARWIN("darwin"), GNU_LINUX("linux"),
+        SOLARIS("solaris"), GNU_SOLARIS("gnu-solaris"), FREEBSD("freebsd"),
+        GNU_FREEBSD("gnu-freebsd"), NETBSD("netbsd"), GNU_NETBSD("gnu-netbsd"),
+        OPENBSD("openbsd"), DRAGONFLYBSD("dflybsd"), HP_UX("hpux"),
+        IRIX("irix"), UNKNOWN;
+
+        private final String idString;
+
+        private ExecutionEnvironment() {
+            this.idString = null;
+        }
+
+        private ExecutionEnvironment(String idString) {
+            if(idString == null)
+                throw new RuntimeException("Do not pass null.");
+            this.idString = idString;
+        }
+
+        public String getIdentifierString() {
+            return idString;
+        }
+    };
+
+    private static CPUArchitecture getJVMArchitecture() {
+        // Trying to cover all thinkable cases here...
+        // Got some hints from http://lopica.sourceforge.net/os.html
+        final String osArch = System.getProperty("os.arch");
+        if(osArch == null)
+            return CPUArchitecture.UNKNOWN;
+        else if(osArch.equalsIgnoreCase("x86") ||
+                osArch.equalsIgnoreCase("i386") ||
+                osArch.equalsIgnoreCase("i486") ||
+                osArch.equalsIgnoreCase("i586") ||
+                osArch.equalsIgnoreCase("i686"))
+            return CPUArchitecture.I386;
+        else if(osArch.equalsIgnoreCase("amd64") ||
+                osArch.equalsIgnoreCase("x86_64") ||
+                osArch.equalsIgnoreCase("x64"))
+            return CPUArchitecture.AMD64;
+        else if(osArch.equalsIgnoreCase("ia64") ||
+                osArch.equalsIgnoreCase("ia64n"))
+            return CPUArchitecture.IA64;
+        else
+            return CPUArchitecture.UNKNOWN;
+    }
+
+    private static ExecutionEnvironment getExecutionEnvironment() {
+        final String osName = System.getProperty("os.name").toLowerCase();
+        final String osVersion = System.getProperty("os.version").toLowerCase();
+
+        if(osName == null)
+            return ExecutionEnvironment.UNKNOWN;
+        else if(osName.startsWith("windows"))
+            return ExecutionEnvironment.WINDOWS;
+        else if(osName.startsWith("mac os x") ||
+                osName.startsWith("darwin"))
+            return ExecutionEnvironment.DARWIN;
+        else if(osName.toLowerCase().startsWith("linux"))
+            return ExecutionEnvironment.GNU_LINUX;
+        else
+            return ExecutionEnvironment.UNKNOWN;
+    }
+
+    public static String getSystemSpecifier() {
+        CPUArchitecture archId = getJVMArchitecture();
+        ExecutionEnvironment reId = getExecutionEnvironment();
+
+        switch(reId) {
+            case DARWIN:
+                // Darwin has fat binaries, so we don't suffix any specific arch
+                return reId.getIdentifierString();
+            default:
+                return reId.getIdentifierString() + "-" +
+                        archId.getArchitectureString();
+        }
+    }
+
+    /**
+     * Does not check if the system is supported, and just tries to load the approprate library
+     * from the architecture string specified in this system's ArchitectureIdentifier.
+     */
+    private static void loadLibrary() {
+        final CPUArchitecture archId = getJVMArchitecture();
+        final ExecutionEnvironment exId = getExecutionEnvironment();
+        if(archId == CPUArchitecture.UNKNOWN) {
+            System.err.println(System.getProperty("os.arch") + ": " +
+                    "Architecture unknown! Cannot locate corresponding " +
+                    "native I/O library.");
+            throw new RuntimeException("loadLibrary(): CPU architecture unknown!");
+        }
+        else if(exId == ExecutionEnvironment.UNKNOWN) {
+            System.err.println(System.getProperty("os.name") + " / " +
+                    System.getProperty("os.version") + ": " +
+                    "Execution environment unknown! Cannot locate " +
+                    "corresponding native I/O library.");
+            throw new RuntimeException("loadLibrary(): Execution environment " +
+                    "(operating system) unknown!");
+        }
+        else {
+            final String libName = LIBRARY_BASE_NAME + "-" + getSystemSpecifier();
+            try {
+                if(verboseLoadLibrary) System.err.println("Trying to load native library \"" + libName + "\"...");
+                System.loadLibrary(libName);
+                if(verboseLoadLibrary) System.err.println("Native library \"" + libName + "\" successfully loaded.");
+            } catch(UnsatisfiedLinkError e) {
+                System.err.println("ERROR: Native library \"" + libName + "\" failed to load!");
+                System.err.println("java.library.path=\"" + System.getProperty("java.library.path") + "\"");
+                throw e;
+            }
+        }
+    }
+
     private static final Object loadSync = new Object();
     private static boolean loaded = false;
 
@@ -19,7 +164,7 @@ class JNILoader {
         synchronized(loadSync) {
             if(!loaded) {
                 System.err.println("Loading library 'jfuse'...");
-                System.loadLibrary("jfuse");
+                loadLibrary();
                 loaded = true;
                 System.err.println("   done.");
             }
