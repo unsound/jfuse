5d8e6e1762dd56f87b6146858ba6f8bfe1c69c8c
Erik Larsson <catacombae@gmail.com>
2009-07-04 18:19:18 +0200

    One newline added to make things look prettier. :)

--- src/java/org/catacombae/jfuse/FUSE.java
+++ src/java/org/catacombae/jfuse/FUSE.java
@@ -16,6 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 package org.catacombae.jfuse;
 
 import org.catacombae.jfuse.types.fuse26.FUSEContext;
