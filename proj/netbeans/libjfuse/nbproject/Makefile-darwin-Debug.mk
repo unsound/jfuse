4c303699f352a6e0bdde3046c062d73fbb0ed3d4
Erik Larsson <catacombae@gmail.com>
2009-07-10 16:51:01 +0200

    Project updated for the latest additions.

--- proj/netbeans/libjfuse/nbproject/Makefile-darwin-Debug.mk
+++ proj/netbeans/libjfuse/nbproject/Makefile-darwin-Debug.mk
@@ -32,9 +32,11 @@ OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEDirFilContext.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/MacFUSE20Util.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_FUSE_26.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSE26Util.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/macfuse20_module.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEFillDirContext.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/jFUSEContext.o \
@@ -71,55 +73,63 @@ dist/darwin-Debug/libjfuse.dylib: ${OBJECTFILES}
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEDirFilContext.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/FUSEDirFilContext.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEDirFilContext.o ../../../src/native/FUSEDirFilContext.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEDirFilContext.o ../../../src/native/FUSEDirFilContext.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/MacFUSE20Util.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/MacFUSE20Util.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/MacFUSE20Util.o ../../../src/native/MacFUSE20Util.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_FUSE_26.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/org_catacombae_jfuse_FUSE_26.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_FUSE_26.o ../../../src/native/org_catacombae_jfuse_FUSE_26.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_FUSE_26.o ../../../src/native/org_catacombae_jfuse_FUSE_26.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSE26Util.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/FUSE26Util.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSE26Util.o ../../../src/native/FUSE26Util.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSE26Util.o ../../../src/native/FUSE26Util.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.o ../../../src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.o ../../../src/native/org_catacombae_jfuse_types_fuse26_FUSEFillDir.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/macfuse20_module.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/macfuse20_module.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/macfuse20_module.o ../../../src/native/macfuse20_module.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEFillDirContext.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/FUSEFillDirContext.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEFillDirContext.o ../../../src/native/FUSEFillDirContext.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/FUSEFillDirContext.o ../../../src/native/FUSEFillDirContext.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.o ../../../src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.o ../../../src/native/org_catacombae_jfuse_types_fuse26_FUSEDirFil.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/jFUSEContext.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/jFUSEContext.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/jFUSEContext.o ../../../src/native/jFUSEContext.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/jFUSEContext.o ../../../src/native/jFUSEContext.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/common.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/common.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/common.o ../../../src/native/common.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/common.o ../../../src/native/common.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_system_StatConstant.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/org_catacombae_jfuse_types_system_StatConstant.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_system_StatConstant.o ../../../src/native/org_catacombae_jfuse_types_system_StatConstant.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_system_StatConstant.o ../../../src/native/org_catacombae_jfuse_types_system_StatConstant.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/fuse26_module.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/fuse26_module.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/fuse26_module.o ../../../src/native/fuse26_module.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/fuse26_module.o ../../../src/native/fuse26_module.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/JNIUtil.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/JNIUtil.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/JNIUtil.o ../../../src/native/JNIUtil.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/JNIUtil.o ../../../src/native/JNIUtil.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_system_Errno.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/org_catacombae_jfuse_types_system_Errno.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_system_Errno.o ../../../src/native/org_catacombae_jfuse_types_system_Errno.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_types_system_Errno.o ../../../src/native/org_catacombae_jfuse_types_system_Errno.cpp
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_util_FUSEUtil.o: nbproject/Makefile-${CND_CONF}.mk ../../../src/native/org_catacombae_jfuse_util_FUSEUtil.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native
-	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_util_FUSEUtil.o ../../../src/native/org_catacombae_jfuse_util_FUSEUtil.cpp
+	$(COMPILE.cc) -g -Wall -D_FILE_OFFSET_BITS=64 -D__FreeBSD__=10 -I/System/Library/Frameworks/JavaVM.framework/Headers -fPIC  -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/_DOTDOT/src/native/org_catacombae_jfuse_util_FUSEUtil.o ../../../src/native/org_catacombae_jfuse_util_FUSEUtil.cpp
 
 # Subprojects
 .build-subprojects:
