9d10e42b03a0ba4801f56c1269d9031bcb90dd9b
Erik Larsson <catacombae@gmail.com>
2009-07-13 12:49:21 +0200

    Added implementation of the JNILoader library test method.

---
+++ libjfuse/src/org_catacombae_jfuse_JNILoader.cpp
@@ -0,0 +1,9 @@
+#include "org_catacombae_jfuse_JNILoader.h"
+
+/*
+ * Class:     org_catacombae_jfuse_JNILoader
+ * Method:    testLibrary
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_catacombae_jfuse_JNILoader_testLibrary
+  (JNIEnv *, jclass) {}
