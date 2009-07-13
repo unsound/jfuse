d01fd04da088c46d10d2ff68a02652f4064a5a2d
Erik Larsson <catacombae@gmail.com>
2009-07-13 12:35:01 +0200

    Made JNILoader attempt to load a library name with no specifiers before anything else, so that we pick up any locally compiled library first. Also fixed a few debug messages to use the Log class, and reorganized the file slightly.

--- src/java/org/catacombae/jfuse/JNILoader.java
+++ src/java/org/catacombae/jfuse/JNILoader.java
@@ -19,66 +19,25 @@
 
 package org.catacombae.jfuse;
 
+import org.catacombae.jfuse.util.Log;
+
 /**
+ * Helper class for loading the jFUSE native library.
  *
- * @author erik
+ * @author Erik Larsson
  */
 public class JNILoader {
     private static final String LIBRARY_BASE_NAME = "jfuse";
 
+    private static final Object loadSync = new Object();
+    private static boolean loaded = false;
+
+    private JNILoader() { throw new RuntimeException(); }
+
     /**
-     * Set this variable to true if you want some messages printed to stderr when the library is
-     * loaded.
+     * Nop method used to test if the correct library was loaded.
      */
-    public static boolean verboseLoadLibrary = true;
-
-    private enum CPUArchitecture {
-
-        I386("i386"), AMD64("amd64"), IA64("ia64"),
-        POWERPC("ppc32"), POWERPC64("ppc64"),
-        SPARC("sparc32"), SPARC64("sparc64"),
-        MIPS("mips32"), MIPS64("mips64"), ALPHA("alpha"),
-        UNKNOWN;
-
-        private final String idString;
-
-        private CPUArchitecture() {
-            this.idString = null;
-        }
-
-        private CPUArchitecture(String idString) {
-            this.idString = idString;
-        }
-
-        public String getArchitectureString() {
-            return idString;
-        }
-    };
-
-    private enum ExecutionEnvironment {
-
-        WINDOWS("windows"), DARWIN("darwin"), GNU_LINUX("linux"),
-        SOLARIS("solaris"), GNU_SOLARIS("gnu-solaris"), FREEBSD("freebsd"),
-        GNU_FREEBSD("gnu-freebsd"), NETBSD("netbsd"), GNU_NETBSD("gnu-netbsd"),
-        OPENBSD("openbsd"), DRAGONFLYBSD("dflybsd"), HP_UX("hpux"),
-        IRIX("irix"), UNKNOWN;
-
-        private final String idString;
-
-        private ExecutionEnvironment() {
-            this.idString = null;
-        }
-
-        private ExecutionEnvironment(String idString) {
-            if(idString == null)
-                throw new RuntimeException("Do not pass null.");
-            this.idString = idString;
-        }
-
-        public String getIdentifierString() {
-            return idString;
-        }
-    };
+    private static native void testLibrary();
 
     private static CPUArchitecture getJVMArchitecture() {
         // Trying to cover all thinkable cases here...
@@ -141,16 +100,27 @@ public class JNILoader {
      * from the architecture string specified in this system's ArchitectureIdentifier.
      */
     private static void loadLibrary() {
+        try {
+            Log.debug("Trying to load native library \"" + LIBRARY_BASE_NAME + "\"...");
+            System.loadLibrary(LIBRARY_BASE_NAME);
+            testLibrary();
+            Log.debug("Native library \"" + LIBRARY_BASE_NAME + "\" successfully loaded.");
+            return;
+        } catch(UnsatisfiedLinkError e) {
+            Log.debug("No specifically compiled \"" + LIBRARY_BASE_NAME + "\" library available. " +
+                    "Trying to look up a precompiled alternative...");
+        }
+
         final CPUArchitecture archId = getJVMArchitecture();
         final ExecutionEnvironment exId = getExecutionEnvironment();
         if(archId == CPUArchitecture.UNKNOWN) {
-            System.err.println(System.getProperty("os.arch") + ": " +
+            Log.error(System.getProperty("os.arch") + ": " +
                     "Architecture unknown! Cannot locate corresponding " +
                     "native I/O library.");
             throw new RuntimeException("loadLibrary(): CPU architecture unknown!");
         }
         else if(exId == ExecutionEnvironment.UNKNOWN) {
-            System.err.println(System.getProperty("os.name") + " / " +
+            Log.error(System.getProperty("os.name") + " / " +
                     System.getProperty("os.version") + ": " +
                     "Execution environment unknown! Cannot locate " +
                     "corresponding native I/O library.");
@@ -160,21 +130,18 @@ public class JNILoader {
         else {
             final String libName = LIBRARY_BASE_NAME + "-" + getSystemSpecifier();
             try {
-                if(verboseLoadLibrary) System.err.println("Trying to load native library \"" + libName + "\"...");
+                Log.debug("Trying to load native library \"" + libName + "\"...");
                 System.loadLibrary(libName);
-                if(verboseLoadLibrary) System.err.println("Native library \"" + libName + "\" successfully loaded.");
+                testLibrary();
+                Log.debug("Native library \"" + libName + "\" successfully loaded.");
+                return;
             } catch(UnsatisfiedLinkError e) {
-                System.err.println("ERROR: Native library \"" + libName + "\" failed to load!");
-                System.err.println("java.library.path=\"" + System.getProperty("java.library.path") + "\"");
+                Log.error("Native library \"" + libName + "\" failed to load!");
+                Log.error("java.library.path=\"" + System.getProperty("java.library.path") + "\"");
                 throw e;
             }
         }
     }
-
-    private static final Object loadSync = new Object();
-    private static boolean loaded = false;
-
-    private JNILoader() { throw new RuntimeException(); }
 
     public static void ensureLoaded() {
         synchronized(loadSync) {
@@ -186,4 +153,54 @@ public class JNILoader {
             }
         }
     }
+
+    private enum CPUArchitecture {
+
+        I386("i386"), AMD64("amd64"), IA64("ia64"),
+        POWERPC("ppc32"), POWERPC64("ppc64"),
+        SPARC("sparc32"), SPARC64("sparc64"),
+        MIPS("mips32"), MIPS64("mips64"), ALPHA("alpha"),
+        UNKNOWN;
+
+        private final String idString;
+
+        private CPUArchitecture() {
+            this.idString = null;
+        }
+
+        private CPUArchitecture(String idString) {
+            if(idString == null)
+                throw new RuntimeException("Do not pass null.");
+            this.idString = idString;
+        }
+
+        public String getArchitectureString() {
+            return idString;
+        }
+    };
+
+    private enum ExecutionEnvironment {
+
+        WINDOWS("windows"), DARWIN("darwin"), GNU_LINUX("linux"),
+        SOLARIS("solaris"), GNU_SOLARIS("gnu-solaris"), FREEBSD("freebsd"),
+        GNU_FREEBSD("gnu-freebsd"), NETBSD("netbsd"), GNU_NETBSD("gnu-netbsd"),
+        OPENBSD("openbsd"), DRAGONFLYBSD("dflybsd"), HP_UX("hpux"),
+        IRIX("irix"), UNKNOWN;
+
+        private final String idString;
+
+        private ExecutionEnvironment() {
+            this.idString = null;
+        }
+
+        private ExecutionEnvironment(String idString) {
+            if(idString == null)
+                throw new RuntimeException("Do not pass null.");
+            this.idString = idString;
+        }
+
+        public String getIdentifierString() {
+            return idString;
+        }
+    };
 }
