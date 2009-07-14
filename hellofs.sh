f35a056feb6fa62a829e7417d88f46782fb9606b
Erik Larsson <catacombae@gmail.com>
2009-07-14 11:06:06 +0200

    fixed library loading in filesystem startup scripts

--- hellofs.sh
+++ hellofs.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:lib" java -cp lib/jfuse.jar org.catacombae.jfuse.examples.HelloFS "$@"
+case `uname -s` in
+[Dd]arwin*)
+	if test -z "${DYLD_LIBRARY_PATH}"; then
+	    # Set up a sane initial library path.
+	    DYLD_LIBRARY_PATH=$HOME/lib:/usr/local/lib:/usr/lib
+	fi
+	export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:lib"
+	;;
+esac
+
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:lib"
+java -cp lib/jfuse.jar org.catacombae.jfuse.examples.HelloFS "$@"
