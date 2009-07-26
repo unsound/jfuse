d8ecebed916b193d11236df3250df3d31ddf7bf6
Erik Larsson <catacombae@gmail.com>
2009-07-26 19:04:04 +0200

    StatVFS fields made public.

--- src/java/org/catacombae/jfuse/types/system/StatVFS.java
+++ src/java/org/catacombae/jfuse/types/system/StatVFS.java
@@ -26,25 +26,25 @@ package org.catacombae.jfuse.types.system;
  */
 public class StatVFS {
     /** File system block size */
-    long f_bsize;
+    public long f_bsize;
     /** Fundamental file system block size */
-    long f_frsize;
+    public long f_frsize;
     /** Blocks on FS in units of f_frsize */
-    long f_blocks;
+    public long f_blocks;
     /** Free blocks */
-    long f_bfree;
+    public long f_bfree;
     /** Blocks available to non-root */
-    long f_bavail;
+    public long f_bavail;
     /** Total inodes */
-    long f_files;
+    public long f_files;
     /** Free inodes */
-    long f_ffree;
+    public long f_ffree;
     /** Free inodes for non-root */
-    long f_favail;
+    public long f_favail;
     /** Filesystem ID */
-    long f_fsid;
+    public long f_fsid;
     /** Bit mask of values */
-    long f_flag;
+    public long f_flag;
     /** Max file name length */
-    long f_namemax;
+    public long f_namemax;
 }
